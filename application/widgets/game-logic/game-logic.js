527a36492dcaf978d1f5bcc611171ded9377c1ec
fredrik <fredrik.jadebeck@gmail.com>
2017-08-14 18:14:12 +0200

    Small modifications.

--- application/widgets/game-logic/game-logic.js
+++ application/widgets/game-logic/game-logic.js
@@ -3,7 +3,7 @@
  * Released under the MIT license
  * www.github.com/qacwnfq
  */
-const board = [...Array( 50 ).keys()].map( i => Array( 50 ) );
+const board = [ ...fixedArray().keys() ].map( fixedArray );
 
 export const injections = [ 'axEventBus' ];
 export function create( eventBus ) {
@@ -11,18 +11,25 @@ export function create( eventBus ) {
    eventBus.subscribe( 'beginLifecycleRequest', () => {
       // board.forEach( a => a.forEach( b => console.log( b ) ) );
       // TODO add loop and publish to eventbus
-      for( let i=0; i<board.length; i++ ) {
-         for( let j=0; j<board[i].length; j++ ) {
-            if( i%2==0 && j%2==0 ){
-               board[i][j] = 1;
+      for( let i = 0; i < board.length; i++ ) {
+         for( let j = 0; j < board[ i ].length; j++ ) {
+            if( i % 2 === 0 && j % 2 === 0 ){
+               board[ i ][ j ] = 1;
             }
-            else{
-               board[i][j] = 0;
+            else {
+               board[ i ][ j ] = 0;
             }
          }
       }
       eventBus.publish( 'didReplace.board', { board } );
    } );
+   eventBus.subscribe( 'didClick.board', event => {
+      board[ event.x ][ event.y ] = !board[ event.x ][ event.y ];
+      eventBus.publish( 'didReplace.board', { board } );
+   } );
 }
 
 
+function fixedArray(){
+   return Array( 50 );
+}
